a7a30bd927b0a6d74680cd279f81bccafa812494
vorber <vorber@gmail.com>
2023-12-07 23:39:53 +0200

    more clear algo

--- src/day7/Program.fs
+++ src/day7/Program.fs
@@ -5,15 +5,20 @@ let cardOrder (Card c) = "AKQJT98765432*" |> Seq.rev |> Seq.mapi (fun i c -> (c,
 let compareCards c1 c2 = compare (cardOrder c1) (cardOrder c2)
 let isWildCard = (Card '*') |> (=)
 type Hand = Hand of Card seq
+let parseHand: (string->Hand) = Seq.map Card >> Hand
 
 let handOrder cards =
     let cnt = cards |> Seq.filter (isWildCard >> not) |> Seq.countBy id |> Seq.map snd |> Seq.sortDescending //|> List.ofSeq
-    match cnt |> Seq.length, cnt |> Seq.sum with
-    | (0, _) | (1, _)   -> 6
-    | (2, x)            -> cnt |> Seq.head |> (+) (6-x)
-    | (3, x)            -> cnt |> Seq.head |> (+) (5-x)
-    | (4, _)            -> 1
-    | _                 -> 0
+    let mostFrequent = cnt |> Seq.tryHead |> Option.defaultValue 0
+    let wildCnt = cnt |> Seq.sum |> (-) 5
+    match mostFrequent + wildCnt, cnt |> Seq.length with
+    | (5, _) -> 6
+    | (4, 2) -> 5
+    | (3, 2) -> 4
+    | (3, 3) -> 3
+    | (2, 3) -> 2
+    | (2, 4) -> 1
+    | _      -> 0
 
 let compareHands (Hand a) (Hand b) =
     match compare (handOrder a) (handOrder b) with
@@ -27,8 +32,7 @@ let compareRounds a b = compareHands a.Hand b.Hand
 
 let parseLine (line: string) =
     let parts = line.Split(" ") |> Array.map (fun s -> s.Trim())
-    let hand = parts[0] |> Seq.map Card |> Hand
-    {Hand=hand; Bid=int parts[1]}
+    {Hand=parseHand parts[0]; Bid=int parts[1]}
 
 let score: (string seq -> int) = 
     Seq.map parseLine >> Seq.sortWith compareRounds >> Seq.mapi (fun i r -> r.Bid * (i+1)) >> Seq.sum
