dd1fffe8315be588b9434ffa77d87f0b7356c4cd
vorber <vorber@gmail.com>
2023-12-09 23:36:48 +0200

    map + sum -> sumBy, roll folder inside predict

--- src/day9/Program.fs
+++ src/day9/Program.fs
@@ -1,15 +1,16 @@
 ﻿open AOC.Misc
 
-let predict folder = 
+let predict = 
     let allZero = Array.forall ((=) 0)
     let diff: (int array -> int array) = Array.pairwise >> Array.map (fun (a, b) -> b - a)
     let generator s = if (allZero s) then None else Some (s, diff s)
+    let folder = flipLastArgs Array.get 0 >> (-)
     Array.unfold generator >> flipLastArgs (Array.foldBack (folder)) 0
 
+let part1 = Seq.sumBy (Array.rev >> predict)
+let part2 = Seq.sumBy predict
+
 let parseLine = splitOn " " >> Array.map int 
-let folder = flipLastArgs Array.get 0 >> (-)
-let part1 = Seq.map (Array.rev >> predict folder) >> Seq.sum
-let part2 = Seq.map (predict folder) >> Seq.sum
 let r1,r2 = AOC.Inputs.load "2023" "9" |> Async.RunSynchronously |> Seq.map parseLine|> applyBoth part1 part2
 
 printfn "Part1: %i" r1
