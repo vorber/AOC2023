23fe427d80f1cd4085b746ce100b292b29fcd789
vorber <vorber@gmail.com>
2023-12-09 23:23:26 +0200

    refactor

--- src/day9/Program.fs
+++ src/day9/Program.fs
@@ -1,19 +1,16 @@
 ﻿open AOC.Misc
 
-let predict folder= 
+let predict folder = 
     let allZero = Array.forall ((=) 0)
     let diff: (int array -> int array) = Array.pairwise >> Array.map (fun (a, b) -> b - a)
     let generator s = if (allZero s) then None else Some (s, diff s)
     Array.unfold generator >> flipLastArgs (Array.foldBack (folder)) 0
 
 let parseLine = splitOn " " >> Array.map int 
-let solve folder = Seq.map (parseLine >> predict folder) >> Seq.sum
-
-let part1 = solve (fun arr s -> s + (Array.last arr))
-let part2 = solve (fun (arr:int array) s -> arr[0] - s)
-let r1,r2 = AOC.Inputs.load "2023" "9" |> Async.RunSynchronously |> applyBoth part1 part2
+let folder = flipLastArgs Array.get 1 >> (-)
+let part1 = Seq.map (Array.rev >> predict folder) >> Seq.sum
+let part2 = Seq.map (predict folder) >> Seq.sum
+let r1,r2 = AOC.Inputs.load "2023" "9" |> Async.RunSynchronously |> Seq.map parseLine|> applyBoth part1 part2
 
 printfn "Part1: %i" r1
 printfn "Part2: %i" r2
-
-
