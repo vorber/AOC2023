37361fc12b3a739678dd90cb57f94f6978aabe41
vorber <vorber@gmail.com>
2023-12-21 02:45:01 +0200

    day20

--- src/AOC/Inputs.fs
+++ src/AOC/Inputs.fs
@@ -4,6 +4,7 @@ module Inputs  =
     open System
     open System.IO
     open System.Net.Http
+    open FParsec
 
     //TODO: don't read all at once, use streams instead
     let private readFrom = File.ReadAllLinesAsync >> Async.AwaitTask
@@ -11,14 +12,14 @@ module Inputs  =
     let private tryReadFrom file =
         if Path.Exists file then Some (readFrom file) else None
 
-    let saveTo file (data:string option) =
+    let private saveTo file (data:string option) =
         async {
             match data with
             | Some text -> do! File.WriteAllTextAsync(file, text) |> Async.AwaitTask
             | None -> failwith "KABOOM"
         }
 
-    let fetch year day =
+    let private fetch year day =
         let url = sprintf "https://adventofcode.com/%s/day/%s/input" year day
         let token = Environment.GetEnvironmentVariable("AOC_SESSION_TOKEN")
         task {
@@ -48,3 +49,16 @@ module Inputs  =
                 | None -> failwith "no content"
         }
                   
+    let parse year day parser =
+        let home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+        let file = home + "/src/Advent/inputs/" + year + "day" + day + ".input"
+        if file |> Path.Exists |> not then
+            async {
+                let! content = fetch year day
+                do! saveTo file content
+            } |> Async.RunSynchronously
+        else ()
+        match runParserOnFile parser "" file System.Text.Encoding.UTF8 with
+        | Success (res, _, _) -> res
+        | Failure (err, _, _) -> failwithf "Failed to parse: %s" err
+
